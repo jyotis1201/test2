3226be87affcad08d875ce1b3a7edaed091131b5
jyotis1201 <jyotisukhwalwork@gmail.com>
2025-07-20 11:57:53 +0530

    B

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -4,6 +4,6 @@ public class Class1 {
 
 	public static void main(String args[]) {
 		System.out.println("p1 c1 world Technocredits");
-		System.out.println("New line");
+		System.out.println("New line A");
 	}
 }
