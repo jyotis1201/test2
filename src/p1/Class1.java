d57e832a1c1466b46776ac2dec92d0219d4b99d4
jyotis1201 <jyotisukhwalwork@gmail.com>
2025-07-20 11:59:51 +0530

    A

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -4,5 +4,8 @@ public class Class1 {
 
 	public static void main(String args[]) {
 		System.out.println("p1 c1 world");
+		System.out.println("p1 A c1 world");
+		System.out.println("p1 B c1 world");
+		
 	}
 }
